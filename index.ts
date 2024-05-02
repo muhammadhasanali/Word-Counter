3035a1bf5bb81cac0cf093b1237f21a3577cfa14
muhammadhasanali <hassanhamid2010@yahoo.com>
2024-05-02 15:52:03 +0500

    Word Counter done

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer' 
+
+let answer = await inquirer.prompt({
+    name:"sentence",
+    message: "Enter your sentence to count the word",
+    type: "input"
+})
+
+let wordcount = answer.sentence.trim().split(" ")
+
+console.log(`Your sentence word count is ${wordcount.length}`)
+
+console.log(wordcount)
